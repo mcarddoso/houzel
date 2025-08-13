612c9f9163a256c7d5c68dba060c2315553b4529
user-matth <mathuscardoso@gmail.com>
2025-08-13 18:04:46 -0300

    restarting project

--- prisma/migrations/20250513222204_add_titles_and_timestamps/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "chats" ADD COLUMN     "deletedAt" TIMESTAMP(3),
-ADD COLUMN     "description" TEXT,
-ADD COLUMN     "title" TEXT,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
--- AlterTable
-ALTER TABLE "messages" ADD COLUMN     "deletedAt" TIMESTAMP(3),
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
